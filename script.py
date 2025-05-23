1d50334b832c6e50bcb48e56748e7318602f6f4d
himanshu <himanshusoni1234@gmail.com>
2025-05-23 09:58:42 +0530

    init

---
+++ script.py
@@ -0,0 +1,34 @@
+# print("Hello")
+# sum = float(input("a:"))+float(input("b:"))
+# print(sum)
+
+# course = 'Hello hs!'
+#
+# print('Hello' in course)
+
+# a = 2
+# if a > 4:
+#     print("It's greater than 4")
+# if a < 4:
+#     print("It's less than 4" + str(a))
+# print('Done')
+#
+
+# i = 1
+# while i<=1_0:
+#     print(i * '*')
+#     i = i + 1
+
+# letters = ["a", "b", "c", "d", "e"]
+# letters.insert(-1, '1')
+# print(letters)
+# letters.remove('a')
+# print(letters)
+#
+# nums = range(5, 10)
+# for i in nums:
+#     print(i)
+
+
+numbers = (1, 2, 3, 3)
+print(numbers.count(3))
